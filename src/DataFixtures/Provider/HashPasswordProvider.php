8aa2f1063f5b9d76cba3717faa20ca4dd764df0d
FaziGIT <mathis.tsrt@gmail.com>
2024-12-20 17:16:53 +0100

    Feat(fixtures): ending fixtures with AliceBundle

---
+++ src/DataFixtures/Provider/HashPasswordProvider.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\DataFixtures\Provider;
+
+class HashPasswordProvider
+{
+    public static function hashPassword(): string
+    {
+        return '$2y$13$PMUioizqkbf9KFVpggxFnu1NZVnHPcPltfagtny6J6E.GGxHTBo1q';
+    }
+}
