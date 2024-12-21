821cce7f041324b0d25fe1333246dee6b627f284
FaziGIT <mathis.tsrt@gmail.com>
2024-12-21 14:00:38 +0100

    feat(Topics): add delete on Topics

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -14,60 +14,6 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/response')]
 final class CommentController extends AbstractController
 {
-    #[Route(name: 'app_response_index', methods: ['GET'])]
-    public function index(CommentRepository $reponseRepository): Response
-    {
-        return $this->render('comment/index.html.twig', [
-            'responses' => $reponseRepository->findAll(),
-        ]);
-    }
-
-    #[Route('/new', name: 'app_response_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $response = new Response();
-        $form = $this->createForm(CommentType::class, $response);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($response);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_response_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('comment/new.html.twig', [
-            'response' => $response,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_response_show', methods: ['GET'])]
-    public function show(Comment $response): Response
-    {
-        return $this->render('comment/show.html.twig', [
-            'response' => $response,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_response_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Comment $response, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(CommentType::class, $response);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_response_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('comment/edit.html.twig', [
-            'response' => $response,
-            'form' => $form,
-        ]);
-    }
-
     #[Route('/{id}', name: 'app_response_delete', methods: ['POST'])]
     public function delete(Request $request, Comment $response, EntityManagerInterface $entityManager): Response
     {
